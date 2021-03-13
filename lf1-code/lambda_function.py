41d672d8337b168d6597ab7d4e4877fb8cd52c37
anishsaha12 <anishsaha12@gmail.com>
2021-03-13 17:18:04 -0500

    Lambda LF1 and LF2 code upload

--- lf1-code/lambda_function.py
+++ lf1-code/lambda_function.py
@@ -1,19 +1,130 @@
 import boto3
 import json
 import requests
+# import random
+# import cv2
+# import string
+# import numpy as np
+import inflect
 from requests_aws4auth import AWS4Auth
 from elasticsearch import Elasticsearch, RequestsHttpConnection
 
 def lambda_handler(event, context):
     print(event)
+
     
     insert_bucket = event["Records"][0]["s3"]["bucket"]["name"]
     insert_key = event["Records"][0]["s3"]["object"]["key"]
-    custom_labels = []
     
     ho = boto3.client("s3").head_object(Bucket=insert_bucket, Key=insert_key)
     
     client=boto3.client('rekognition')
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table("hw2-faces-index-dynamodb")
+    collection_id_name = "hw2-my-faces-collection"
+
+    try:
+        raw_labels = ho["Metadata"]["customlabels"]
+        custom_labels = raw_labels.split(', ')
+    except:
+        raw_labels = ''
+        custom_labels = []
+
+    # Code based on tutorial: https://aws.amazon.com/blogs/machine-learning/build-your-own-face-recognition-service-using-amazon-rekognition/    
+    if 'name_' in raw_labels:                               # if user provides name
+        input_name_tag = custom_labels[-1][5:]
+        custom_labels = custom_labels[:-1] + [input_name_tag]
+        print('name', input_name_tag)
+        
+        response = client.index_faces(
+            Image={
+                "S3Object": {
+                    "Bucket": insert_bucket,
+                    "Name": insert_key
+                }
+            },
+            CollectionId=collection_id_name
+        )
+                
+        print("rekognition_client response : {}".format(response))
+                
+        face_id = response['FaceRecords'][0]['Face']['FaceId']
+    
+        r = {
+            "FaceId": face_id,
+            "name_tag": input_name_tag
+        }
+        
+        response = table.put_item(Item=r)
+        
+        print("dynamodb put response :{}".format(response))
+    
+    else:                   # user doesn't provide name -> auto detect name if existing already
+        response = client.search_faces_by_image(
+            CollectionId=collection_id_name,
+    		Image={
+    		    "S3Object":{"Bucket": insert_bucket, "Name": insert_key}
+    		}
+        )
+    
+        detected_name_tags = []
+        
+        for match in response['FaceMatches']:
+            print(match['Face']['FaceId'],match['Face']['Confidence'])
+            f_id = match['Face']['FaceId']
+            response = table.get_item(Key={'FaceId': f_id})
+            detected_name_tags.append(response['Item']['name_tag'])
+        
+        custom_labels += detected_name_tags
+        print("detected_name_tags: {}".format(detected_name_tags))
+        # insert_processing_bucket = "hw2-processing-bucket" 
+        # s3_resource = boto3.resource('s3')
+    
+        # bucket = s3_resource.Bucket(insert_bucket)
+        # img = bucket.Object(insert_key).get().get('Body').read()
+        # image = cv2.imdecode(np.asarray(bytearray(img)), cv2.IMREAD_COLOR)
+    
+        # response = client.detect_faces(Image={"S3Object":{"Bucket": insert_bucket, "Name": insert_key}})
+        # all_faces=response['FaceDetails']
+    
+    
+        # image_width = np.shape(image)[1]
+        # image_height = np.shape(image)[0]
+    
+        # detected_name_tags = []
+    
+        # for face in all_faces:
+        #     box=face['BoundingBox']
+        #     x1 = int((box['Left'] * image_width) * 0.9)
+        #     y1 = int((box['Top'] * image_height) * 0.9)
+        #     x2 = int((box['Left'] * image_width + box['Width'] * image_width) * 1.10)
+        #     y2 = int((box['Top'] * image_height + box['Height']  * image_height) * 1.10)
+    
+        #     image_crop = image[y1:y2, x1:x2]
+    
+        #     data_serial = cv2.imencode('.png', image_crop)[1].tostring()
+            
+        #     letters = string.ascii_lowercase
+        #     f_name = ''.join(random.choice(letters) for i in range(10)) +'.png'
+            
+        #     s3_resource.Object(insert_processing_bucket, f_name).put(Body=data_serial,ContentType='image/PNG')
+    
+        #     response = client.search_faces_by_image(
+        #         CollectionId=collection_id_name,
+        #         Image={"S3Object":{"Bucket": insert_processing_bucket, "Name": f_name}}
+        #         )
+    
+        #     # s3_resource.Object(insert_processing_bucket, f_name).delete()
+    
+        #     for match in response['FaceMatches']:
+        #         f_id = match['Face']['FaceId']
+        #         response = table.get_item(Key={'FaceId': f_id})
+        #         detected_name_tags.append(response['Item']['name_tag'])
+        
+        # custom_labels += detected_name_tags
+        # print(detected_name_tags)
+        
+    print('all_c_labels', custom_labels)
 
     response = client.detect_labels(Image={'S3Object':{'Bucket':insert_bucket,'Name':insert_key}},
         MaxLabels=50)
@@ -25,6 +136,18 @@ def lambda_handler(event, context):
         
     all_labels = detected_labels + custom_labels
     
+    ## Convert Plurals to Singulars
+    inflect_p = inflect.engine()
+    final_labels = []
+    for label in all_labels:
+        res = inflect_p.singular_noun(label)
+        if res==False:
+            final_labels.append(label)
+        else:
+            final_labels.append(res)
+    
+    print('All Labels Final (after inflection):', final_labels)
+    
     timestamp_datetime = ho["LastModified"]
     timestamp_string = timestamp_datetime.strftime("%Y-%m-%dT%H:%M:%S")
 
@@ -33,8 +156,8 @@ def lambda_handler(event, context):
         "objectKey": insert_key,
         "bucket": insert_bucket,
         "createdTimestamp": timestamp_string,
-        "labels": all_labels
-        }
+        "labels": final_labels
+    }
         
     print(es_json)
     
---
+++ lf2-code/lambda_function.py
@@ -0,0 +1,154 @@
+import boto3
+import json
+import inflect
+import requests
+from requests_aws4auth import AWS4Auth
+
+def lambda_handler(event, context):
+    print(event['params']['querystring']['q'])
+    client = boto3.client('lex-runtime')
+    
+    data = event['params']['querystring']['q']
+    # data = "Show me a cat"
+    
+    response = client.post_text(
+        botName='disambiguator',
+        botAlias='prod',
+        userId='id1',
+        inputText= data
+    )
+    
+    labels_ini = []
+    
+    try:
+        if response["intentName"] == "searchintent":
+            for slot in response["slots"]:
+                e = response["slots"][slot]
+                if e is not None:
+                    labels_ini.append(e)
+    except:
+        labels_ini = []
+        
+    ## Convert Plurals to Singulars
+    inflect_p = inflect.engine()
+    labels = []
+    for label in labels_ini:
+        res = inflect_p.singular_noun(label)
+        if res==False:
+            labels.append(label)
+        else:
+            labels.append(res)
+    
+    print('Labels (after inflection):', labels)
+
+    
+    # labels = ["cat", "dog"]
+    # labels = ["dog"]
+
+
+    # region = 'us-east-1'
+    # service = 'es'
+    # credentials = boto3.Session().get_credentials()
+    # awsauth = AWS4Auth(credentials.access_key, credentials.secret_key, region, service, session_token=credentials.token)
+    # host = 'search-photos-7ka2227iannxvqedwmlx62ft7u.us-east-1.es.amazonaws.com' # For example, search-mydomain-id.us-west-1.es.amazonaws.com
+    # index = 'photos'
+    # url = 'https://' + host + '/' + index + '/_search'
+    # headers = { "Content-Type": "application/json" }
+    
+
+    # es_query_string = ""
+    # if len(labels) >=1 :
+    #     es_query_string = "(" + labels[0] + ")"
+    #     for i in range(len(labels) - 1):
+    #         es_query_string = es_query_string + " OR (" + labels[i+1] + ")"
+            
+            
+
+    # query = {
+    #     "size": 10,        
+    #     "query": {
+    #         "query_string": {
+    #           "default_field": "labels",
+    #           "query": es_query_string
+    #         }
+    #       }
+    # }
+    
+    # r = requests.get(url, auth=awsauth, headers=headers, data=json.dumps(query)).json()
+    
+    # search_result_list = r["hits"]["hits"]
+    
+    matching_images_loc = []
+    
+    search_result_list = [
+        {
+            "_source": {
+                "objectKey": "116227.jpg",
+                "bucket": "hw2-b2-photos",
+                "labels": [
+                    "Person",
+                    "Human",
+                    "Building",
+                    "Campus",
+                    "Urban"
+                ]
+            }
+        },
+        {
+            "_source": {
+                "objectKey": "116341.jpg",
+                "bucket": "hw2-b2-photos",
+                "labels": [
+                    "People",
+                    "City",
+                    "Town",
+                    "Downtown"
+                ]
+            }
+        },
+        {
+            "_source": {
+                "objectKey": "117173.jpg",
+                "bucket": "hw2-b2-photos",
+                "labels": [
+                    "Architecture",
+                    "Grass",
+                    "Plant",
+                    "Field"
+                ]
+            }
+        },
+        {
+            "_source": {
+                "objectKey": "117346.jpg",
+                "bucket": "hw2-b2-photos",
+                "labels": [
+                    "Vegetation",
+                    "Amphitheatre",
+                    "Arena",
+                    "Amphitheater",
+                ]
+            }
+        }
+    ]
+    
+    for result in search_result_list:
+        image_name = result["_source"]["objectKey"]
+        bucket_name = result["_source"]["bucket"]
+        labels_list = result["_source"]["labels"]
+        
+        matching_images_loc.append({
+            'url': 'https://'+bucket_name+'.s3.amazonaws.com/'+image_name,
+            'labels': labels_list
+        })
+        
+    print(matching_images_loc)
+
+    return {
+        'results': matching_images_loc,
+    }
+
+
+
+
+
